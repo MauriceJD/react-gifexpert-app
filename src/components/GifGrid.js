b0b5b2004d02ddfcd1a3e0787ac1309ba5ecbdd8
Joseph <zero2341@hotmail.com>
2020-10-29 21:52:08 -0600

    jhjfgjhj

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
 export const GifGrid = ({category}) => {
+
+    const GetGifs = async() => {
+
+        const url = 'https://api.giphy.com/v1/gifs/search?q=Naruto&limit=5&api_key=9r6PyHG46cRHiSA2HVRJF8Q2YSIErUV2'
+        const resp = await fetch(url);
+        const {data} = await resp.json();
+
+        const gifs = data.map( img => {
+            return {
+                id: img.id,
+                title: img.title,
+                url: img.images.downsized_medium.url
+            }
+        })
+        
+        console.log(gifs);
+
+    }
+
+    GetGifs();
+
     return (
         <div>
            <h3>{category}</h3> 
