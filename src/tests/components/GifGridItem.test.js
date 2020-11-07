b79385cedf68c59a9926c3ea18551365f34b7681
Joseph Delille <m_j_d_l_@outlook.com>
2020-11-07 12:07:12 -0600

    hola

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -4,14 +4,44 @@ import GifGridItem from '../../components/GifGridItem';
 
 
 describe('Pruebas en el archivo GifGridItem.js', () => {
+    
+    
+    const title = 'Hola que tal saludos';
+    const url = 'https://localhost/algo.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>)
+    
+    
     test('debe mostrar el componente correctamente', () => {
-
-        const title = 'Hola que tal saludos';
-        const url = 'https://localhost/algo.jpg'
-
-        const wrapper = shallow(<GifGridItem title={title} url={url}/>)
+        
         expect(wrapper).toMatchSnapshot()
+        
     })
+    
+    test('debe mostrar un parrafo con el title', () => {
+        
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title)
+        
+    })
+    
+    test('debe tener como atributos el src y el titulo en img', () => {
+        
+        const img = wrapper.find('img');
+        expect(img.prop('src')).toBe(url)
+        expect(img.prop('alt')).toBe(title)
+        
+    })
+    
+    test('div debe de tener la clase de animacion', () => {
+        
+        const div = wrapper.find('div');
+        const classname = div.prop('className');
+        expect(div.prop('className')).toBe(classname)
+        
+    })
+    
+    
+    
     
 })
 
